6ae1730f969ec49ef6830f6efff5a13a91574ace
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-05 15:59:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double flag = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    flag = flag * value;
+  }
+  return flag;
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * fact(n-1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  double flag = 1;
+  for (int i = 0; i < count; i++) {
+    flag = flag + calcItem(x, i);
+  }
+  return flag;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double flag = x;
+  for (int i = 2; i <= count; i++) {
+    flag = flag + pown(-1, i-1) * calcItem(x, i * 2 - 1);
+  }
+  return flag;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double flag = 1;
+  for (int i = 2; i <= count; i++) {
+    flag = flag + pown(-1, i-1) * calcItem(x, i * 2 - 2);
+  }
+  return flag;
 }
